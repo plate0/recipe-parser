b2e99a25dc3ee618aaaebc29230634be1528f683
Ethan Mick <ethan@ethanmick.com>
2019-06-24 14:15:44 -0400

    Update NYT Cooking Parser
    1. Fix ingredient parser to not get optional nutritional info
    2. Add in better description parser

--- parsers/cooking.nytimes.com.js
+++ parsers/cooking.nytimes.com.js
@@ -1,4 +1,5 @@
 const moment = require('moment')
+const { description } = require('../src')
 
 exports.yield = $ =>
   $('ul.recipe-time-yield li')
@@ -8,9 +9,13 @@ exports.yield = $ =>
     .text()
     .trim()
 
+exports.description = $ =>
+  description($('[itemprop="description"] p').first())($)
+
 exports.duration = $ =>
   moment.duration($('meta[itemprop="cookTime"]').attr('content')).asSeconds()
 
 exports.preheats = '.recipe-steps'
-exports.ingredient_lists = 'ul.recipe-ingredients li'
+exports.ingredient_lists =
+  'ul.recipe-ingredients li[itemprop="recipeIngredient"]'
 exports.procedure_lists = 'ol.recipe-steps li'
