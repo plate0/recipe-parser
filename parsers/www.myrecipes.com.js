faf2556d931053a44a880df0987c21b8b6b88aed
Brian Dobby <brian@blivot.com>
2019-09-06 16:31:48 +0100

    All test run except for image_url

--- parsers/www.myrecipes.com.js
+++ parsers/www.myrecipes.com.js
@@ -1,27 +1,39 @@
-/**
- * @jest-environment node
- */
-const _ = require('lodash')
-const axios = require('axios')
-const adapter = require('axios/lib/adapters/http')
+const { parseIngredient } = require('ingredient-parser')
+const _ = require( 'lodash' )
+const axios = require( 'axios' )
+const adapter = require( 'axios/lib/adapters/http' )
 
-exports.fetch =  async (url, options={}) => {
+exports.fetch = async ( url, options ) => {
+  console.error( `url ${typeof url}, options ${JSON.stringify( options )}` )
   let config = {
-      headers: {
-        Cookie: "euConsent=true; euConsentId=79af88de-7209-4335-8d2f-c3d328e5d812"
-      },
-      transformRequest: logHeaders,
-      adapter: adapter
+    headers: {
+      Cookie: "euConsent=true; euConsentId=79af88de-7209-4335-8d2f-c3d328e5d812"
+    },
+    adapter: adapter
   }
-  _.merge(options, config)
-  return axios.get(url, options)
+  _.merge( options, config )
+  try {
+    return await axios.get( url, options )
+  } catch ( err ) {
+    console.error( `url ${typeof url}, options ${JSON.stringify( options )}` )
+    console.error( err )
+  }
 }
 
-function logHeaders(data, headers) {
-  console.log(`\x1b[97m${JSON.stringify(headers)}\x1b[0m`)
+exports.description = 'div.recipe-summary div p'
+exports.procedure_lists = 'div.step p'
+exports.yield = 'div.recipe-meta-item-body'
+exports.image_url = 'div.recipe-content div.image-container div.inner-container img'
+
+exports.ingredient_lists = $ => {
+  return [
+    {
+      name: undefined,
+      image_url: undefined,
+      lines: $( 'div.ingredients ul li' )
+        .map( function() {
+          return parseIngredient($(this).text())
+        } ).get()
+    }]
 }
 
-
-var config = {
-  url: 'https://google.com',
-}
--- src/parser.js
+++ src/parser.js
@@ -7,30 +7,49 @@ const html = require('./html')
  */
 const dom = func => (html, ...rest) => func(cheerio.load(html), ...rest)
 
-/*
- *
+/**
+ * Transform the given value and key into a function 
+ * @param val the value of the property specified by 'key';
+ *            may be a string or a function
+ * @param key the key to 'value' in the object being traversed;
+ *            should be one of the exports from html.js
+ * @return a function to retrieve the recipe component indicated by 'key'
  */
-const handler = (val, key) =>
-  typeof val === 'function' ? val : (html[key] || html.text)(val)
-
-/*
- *
+const handler = (val, key) => {
+//  console.log(`handler: ${key} ${typeof val}`)
+  return typeof val === 'function' ? val : (html[key] || html.text)(val)
+}
+/**
+ * Transform each value in 'object' to the result of calling it as a function
+ * @param object the object
+ * @return the transformed object
  */
 const mapValues = object => (...args) =>
   _.mapValues(object, v => (v ? v(...args) : undefined))
 
-/*
- * A Parser expects DOM (Cheerio loaded) and returns a parsed recipe
+/**
+ * Returns a parser based on the given 'partial' parser.
+ * 
+ * @param partial Either a function to parse html to a recipe
+ *                or     a map of recipe component names to functions or selectors
  *
- * If a function, knows what it is doing, needs to call dom(...)
+ * If 'partial' is a function, it needs to 
+ *    1. take arguments (html, {options})
+ *    2. construct and return a PostRecipe
+ *    
+ * Otherwise, 'partial' should be as described in README.md
+ * 
+ * @return a function to parse html to a recipe
  */
-const parser = partial =>
-  typeof partial === 'function'
+const parser = partial => {
+  return typeof partial === 'function'
     ? partial
     : dom(mapValues(_.mapValues({ ...html.defaults, ...partial }, handler)))
+}
 
 module.exports = {
   dom,
   mapValues,
-  parser
+  parser,
+  handler
 }
